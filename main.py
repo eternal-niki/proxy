ab642b4580886349ad81efd1ce464a78feb4dbfe
eternal-niki <eternal_niki@hotmail.com>
2025-09-05 22:35:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -215,5 +215,5 @@ def proxy():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))  # ← ここでportを定義
-    app.run(host="0.0.0.0", port=port)
+    #port = int(os.environ.get("PORT", 8080))  # ← ここでportを定義
+    app.run(host="0.0.0.0", port=8080)
